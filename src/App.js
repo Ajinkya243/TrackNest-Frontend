d73a273988b35a0cabe755ae4a7e929fbb730b1d
Ajinkya243 <ajinkyagund2@gmail.com>
2025-04-04 16:51:03 +0530

    initial code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import Sidebar from './components/sidebar/Sidebar';
+import { useDispatch } from 'react-redux';
+import { fetchLeads, pipelineLeads } from './utils/redux/slice/leadsSlice';
+import Dashboard from './components/Dashboard/Dashboard';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const dispatch=useDispatch();
+  useEffect(()=>{
+    dispatch(fetchLeads());
+    dispatch(pipelineLeads());
+  })
+  return(
+    <>
+    <Navbar/>
+    <Sidebar/>
+    <Dashboard/>
+    </>
+  )
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,46 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {Provider} from 'react-redux'
+import store from './utils/redux/store/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor } from './utils/redux/store/store';
+import GlobalStateProvider from './utils/context/GlobalStateProvider';
+import LeadsPage from './pages/leads page/LeadsPage';
+import AgentsPage from './pages/agents page/AgentsPage';
+import SalesPage from './pages/sales page/SalesPage';
+import ReportPage from './pages/report page/ReportPage';
+import AddAgent from './pages/add agent/AddAgent';
+import {ToastContainer} from 'react-toastify';
+import Login from './pages/login page/Login';
+import Profile from './pages/profile page/Profile';
+import AgentDetails from './pages/agents page/AgentDetails';
+import LeadsDetails from './pages/leads page/LeadsDetails';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}> 
+      <GlobalStateProvider>
+    <Routes> 
+      <Route path="/" element={<App/>}/>
+      <Route path="/leads" element={<LeadsPage/>}/>
+      <Route path="/agents" element={<AgentsPage/>} />
+      <Route path="/sales" element={<SalesPage/>}/>
+      <Route path="/report" element={<ReportPage/>} />
+      <Route path="/add-agent" element={<AddAgent/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+      <Route path="/agents/details" element={<AgentDetails/>} />
+      <Route path="/leads/details/:id" element={<LeadsDetails/>} />
+    </Routes>
+    </GlobalStateProvider>
+    </PersistGate>
+    </Provider>
+    </BrowserRouter>
+    <ToastContainer/>
   </React.StrictMode>
 );
 
